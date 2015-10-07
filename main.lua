1aa03e8bec3293f2867979bf9f169058369925dc
Bobbyjones <davidantonioserrano66@gmail.com>
2015-10-07 18:01:26 -0400

    separated some more stuff

--- main.lua
+++ main.lua
@@ -2,72 +2,29 @@ local lurker      = require 'libs.lurker.lurker'
 local lume        = require 'libs.lume.lume'
 local json        = require 'libs.dkjson'
 local tserial     = require 'libs.tserial'
+local State       = require 'libs.hump.gamestate'
 
 local utils       = require 'utils'
-local gui         = require 'gui'
-local theme       = require 'themes.defualt'
-local TodoAPI     = require 'APIs.Todo'
+local listState   = require 'state.list'
 
-
-local fontManager = gui.fontManager()
-
-list = gui.list(0,0,400,600)
-
-local todos, todosJson
-local updateRate = 1/10
-local counter    = 0
-local timeStamp  = 0
-local newStamp   = 0
-local url        = "http://localhost:5000"
-local api        = {} -- api namespace
-api.todo   = TodoAPI( url, 1)
 
 
 function love.load( ) 
 
-	testJson = [[{ "task" :  "task ]]..love.math.random()..[["}]]
-
- 	local res = api.todo:addTodo( testJson )
-
- 	todos, newStamp = api.todo:getTodos()
-	
 	love.graphics.setBackgroundColor( 255,255,255 )
-
+	State.switch(listState)
+	State.registerEvents()
 
 end
 
 function love.update( dt )
-
 	lurker.update()
-	if todos == nil then 
-		todos, newStamp = api.todo:getTodos()
-
-	end
-
-	counter = counter + dt
-	if counter >= updateRate then
-		todos, newStamp = api.todo:getTodos()
-
-		counter = 0
-	end
-
-	if newStamp > timeStamp then
-		for i, task in ipairs( todos ) do
-			local _task = gui.task(i,task.task)
-			list:addTask(_task)
-		end
-		timeStamp = newStamp
-	end
-	list:update()
 end
 
 
 
 function love.draw( )
 
-	list:draw(0,theme,fontManager)
-	love.graphics.setColor({0,0,0})
-	love.graphics.setFont(love.graphics.newFont(12))
-	love.graphics.print(love.timer.getFPS())
+	
 
 end
---
+++ state/list.lua
@@ -0,0 +1,69 @@
+local gui         = require 'gui'
+local theme       = require 'themes.defualt'
+local TodoAPI     = require 'APIs.Todo'
+
+local todos, todosJson
+local updateRate = 1/10
+local counter    = 0
+local timeStamp  = 0
+local newStamp   = 0
+local url        = "http://localhost:5000"
+local fontManager = gui.fontManager()
+local api        = {} -- api namespace
+api.todo   = TodoAPI( url, 1)
+
+list = gui.list(0,0,400,600)
+List = {}
+
+function List:init() -- run only once
+    
+end
+
+function List:enter(previous) -- run every time the state is entered
+	testJson = [[{ "task" :  "task ]]..love.math.random()..[["}]]
+
+ 	local res = api.todo:addTodo( testJson )
+
+ 	todos, newStamp = api.todo:getTodos()
+
+end
+
+function List:update(dt)
+	if todos == nil then 
+		todos, newStamp = api.todo:getTodos()
+
+	end
+
+	counter = counter + dt
+	if counter >= updateRate then
+		todos, newStamp = api.todo:getTodos()
+
+		counter = 0
+	end
+
+	if newStamp > timeStamp then
+		for i, task in ipairs( todos ) do
+			local _task = gui.task(i,task.task)
+			list:addTask(_task)
+		end
+		timeStamp = newStamp
+	end
+	list:update()
+end
+
+function List:draw()
+	list:draw(0,theme,fontManager)
+	love.graphics.setColor({0,0,0})
+	love.graphics.setFont(love.graphics.newFont(12))
+	love.graphics.print(love.timer.getFPS())
+end
+
+function List:keyreleased(key)
+
+end
+
+function List:mousereleased(x,y, mouse_btn)
+    
+end
+
+return List
